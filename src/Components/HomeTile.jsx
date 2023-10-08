6095e50602fda54a58bfecf6bd4f295416e96c54
NavinD1234 <139956446+NavinD1234@users.noreply.github.com>
2023-10-08 20:04:43 +0530

    Update HomeTile.jsx

--- src/Components/HomeTile.jsx
+++ src/Components/HomeTile.jsx
@@ -1,8 +1,51 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import backimg from "../images/7ec0559f-6180-4b29-becd-0f12f702ec5c._UR3840,1440_SX1440_FMjpg_.jpeg";
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 function ListItem(props) {
+  const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [data3, setData3] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const token = localStorage.getItem('jwtToken');
+  const projectId = '24ivifnsynsd';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://academics.newtonschool.co/api/v1/ott/show', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            'projectId': projectId,
+          },
+        });
+        const last5Data = response.data.data.slice(-15);
+
+        setData2(last5Data);
+
+
+        const last5Data3 = response.data.data.slice(-7);
+
+        setData3(last5Data3);
+        //setIsLoading(false);
+    
+        setData(response.data.data);
+        setIsLoading(false);
+
+        console.log('Fetched Data:', response.data);
+      } catch (error) {
+        console.error('Fetch Data Error', error);
+      }
+    };
+
+    fetchData();
+  }, [token, projectId]);
+
+
   // const [isVisible, setisVisible] = useState(false);
 
   // function ObserveFirstChild(event) {
@@ -34,6 +77,8 @@ function ListItem(props) {
   // }, 1000);
 
   return (
+    <>
+    {data3.map((movie) => (
     <li
       style={{
         aspectRatio: "3/1",
@@ -50,6 +95,7 @@ function ListItem(props) {
           aspectRatio: "3/1",
         }}
       >
+      
         <div className="abc">
           <div
             style={{
@@ -62,7 +108,7 @@ function ListItem(props) {
           >
             <img
               style={{ height: "100%", width: "100%" }}
-              src={props.poster}
+              src={movie.thumbnail}
               alt="background color for now."
             ></img>
           </div>
@@ -72,9 +118,10 @@ function ListItem(props) {
                 <div className="home-tile-details-img-div">
                   <img
                     className="home-tile-details-img"
-                    src={props.images}
+                    // src={props.images}
+                    // src={movie.thumbnail}
                     // src={props.logo}
-                    alt="tite art"
+                    // alt="tite art"
                   ></img>
                 </div>
               </a>
@@ -83,7 +130,7 @@ function ListItem(props) {
               <div className="home-tile-details-notposter-div2">
                 <span className="home-tile-details-notposter-span1">
                   {/* basic information */}
-                  {props.released}
+                  {props.createdAt}
                 </span>
                 <span className="home-tile-details-notposter-span2">
                   {props.rated}
@@ -93,7 +140,12 @@ function ListItem(props) {
                 <div className="home-tile-details-buttons-play-div1">
                   <div className="home-tile-details-buttons-play-div2 ">
                     <label className="home-tile-details-buttons-play-label">
-                      <a className="home-tile-details-buttons-play-a" href="">
+                    
+                    <Link
+                      key={movie._id}
+                      to={`/details/${movie._id}`} // Ensure movie._id is correct
+                      className="movie-card"
+                    >
                         <svg
                           className="_22qEau"
                           viewBox="0 0 80 80"
@@ -117,11 +169,26 @@ function ListItem(props) {
                           </svg>
                           <title>Play Button</title>
                         </svg>
-                      </a>
-                      <span>Play</span>
+                    
+
+
+                      </Link>
+
+
+
+                      <Link
+  key={movie?._id} // Use optional chaining to prevent errors if movie is undefined
+  to={movie ? `/details/${movie._id}` : '/'} // Redirect to a default route or handle the case when movie is undefined
+  className="movie-card"
+>
+  {movie ? <span style={{ color: "#fff" }}>Play</span> : null}
+</Link>
+
                     </label>
+
                   </div>
                 </div>
+               
                 <div className="home-tile-details-buttons-watchlist-div">
                   <button className="home-tile-details-buttons-watchlist-button">
                     <svg
@@ -153,6 +220,8 @@ function ListItem(props) {
                     Watchlist ***xDisplayed on Hover***
                   </label>
                 </div>
+               
+               
                 <div className="home-tile-details-buttons-info-div">
                   <a href="" className="home-tile-details-buttons-info-a">
                     <svg
@@ -190,12 +259,16 @@ function ListItem(props) {
                     Details
                   </label>
                 </div>
-              </div>
+               
+              </div>  
             </div>
           </section>
         </div>
+        
       </article>
     </li>
+    ))}
+    </>
   );
 }
 
